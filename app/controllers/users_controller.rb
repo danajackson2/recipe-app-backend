dc222e99e9bdab42e0390c01e59eaf3f6ad62620
danajackson2 <danajackson2@gmail.com>
2021-04-30 12:40:34 -0700

    backedn login signup logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create]
+
+    def create
+        user = User.create(user_params)
+        byebug
+        if user.valid?
+            token = encode_token({user_id: user.id})
+            render json: {username: user.username, user_id: user.id, token: token}
+        else
+            render json: {error: 'Failed to create user'}, status: :not_acceptable
+        end
+    end
+
+    private
+    
+    def user_params
+        params.require(:new_user).permit(:username, :password)
+    end
+
 end
